179d7f781b24ce565cb500748489f37a280f4375
Mika6413 <michaebony@gmail.com>
2024-05-07 23:11:37 +0200

    dashboard skeleton

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -5,7 +5,7 @@ import {
   InboxIcon,
 } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
-import { Card } from '@/app/ui/dashboard/cards';
+
 
 const iconMap = {
   collected: BanknotesIcon,
